a84e0a5a4ff7b9e769178485cb479585249f3935
jadins24 <jadinesh04@gmail.com>
2022-12-27 14:57:54 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const passengers = [
+    {
+      id: 1,
+      passengerName: "Freddie Mercury",
+      isVegetarianOrVegan: false,
+      connectedFlights: 2,
+    },
+    {
+      id: 2,
+      passengerName: "Amy Winehouse",
+      isVegetarianOrVegan: true,
+      connectedFlights: 4,
+    },
+      {
+      id: 3,
+      passengerName: "Kurt Cobain",
+      isVegetarianOrVegan: true,
+      connectedFlights: 3,
+    },
+       {
+      id: 3,
+      passengerName: "Michael Jackson",
+      isVegetarianOrVegan: true,
+      connectedFlights: 1,
+    },
+  ];
+
+ var a=[];
+   for (i=0; i<passengers.length; i++){
+            a[i]=passengers[i].passengerName;
+
+   }
+   console.log(a);
